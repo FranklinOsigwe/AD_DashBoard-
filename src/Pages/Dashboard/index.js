07b00bf26617d96755936c2381ec9f243c1458a0
Franklin Osigwe <franklindutch1@gmail.com>
2023-04-22 22:20:16 +0100

    graph done

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import {Typography} from 'antd'  
+import React from "react";
+import { Typography, Card, Space, Statistic } from "antd";
+import {
+  ShoppingCartOutlined,
+  UserOutlined,
+  DollarCirlceOutlined,
+  ShoppingOutlined,
+} from "@ant-design/icons";
 
-function Customers() {
+function Dashboard() {
   return (
     <div>
-      <Typography.Title level={4}>Customers</Typography.Title>
+      <Typography.Title level={4}>Dashboard</Typography.Title>
+      <Space direction="horizontal">
+        <Card>
+          <Space direction="horizontal">
+            <DashboardCard
+              icon={<ShoppingCartOutlined />}
+              title={"Orders"}
+              value={1234}
+            />
+            <DashboardCard title={"Orders"} value={1234} />
+            <DashboardCard title={"Customer"} value={1234} />
+            <DashboardCard title={"Inventory"} value={1234} />
+            <DashboardCard title={"Revenue"} value={1234} />
+          </Space>
+        </Card>
+      </Space>
     </div>
-  )
+  );
 }
 
-export default Customers
+function DashboardCard({ title, value }) {
+  return (
+    <div>
+      <Card>
+        <Space direction="horizontal">
+          <Statistic title={title} value={value} />
+        </Space>
+      </Card>
+    </div>
+  );
+}
+
+export default Dashboard;
